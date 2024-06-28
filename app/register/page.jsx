122c1efeb627d2bffd21847dc3c79d6e2ff6f26f
sakib-xrz <sakibxrz21@gmail.com>
2024-06-28 22:23:15 +0600

    Refactor registration page to use Formik for form handling and add validation

--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,3 +1,10 @@
+"use client";
+
+import * as Yup from "yup";
+import { useState } from "react";
+import { useFormik } from "formik";
+import { toast } from "sonner";
+import { useRouter, useSearchParams } from "next/navigation";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
@@ -10,10 +17,62 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import FormikError from "@/components/shared/FormikError";
+
+import ApiKit from "@/common/ApiKit";
+import { setTokenAndRedirect } from "@/lib/utils";
 
 export default function ResisterForm() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const previousURL = searchParams.get("next");
+  const [loading, setLoading] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("name is required"),
+      email: Yup.string().email().required("email is required"),
+      password: Yup.string().required("password is required"),
+    }),
+    onSubmit: (values) => {
+      setLoading(true);
+
+      const promise = ApiKit.auth
+        .register(values)
+        .then(({ data }) => {
+          setTokenAndRedirect(data.access, () => {
+            if (previousURL) {
+              router.push(previousURL);
+            } else {
+              router.push("/");
+            }
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          throw error;
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+
+      toast.promise(promise, {
+        loading: "Creating an account...",
+        success: "Account created successfully",
+        error: (error) => error?.message || "Failed to create an account",
+      });
+    },
+  });
   return (
-    <div className="flex min-h-screen w-full items-center justify-center max-sm:px-4">
+    <form
+      onSubmit={formik.handleSubmit}
+      className="flex min-h-screen w-full items-center justify-center max-sm:px-4"
+    >
       <Card className="mx-auto max-w-sm">
         <CardHeader>
           <CardTitle className="text-xl">Sign Up</CardTitle>
@@ -23,15 +82,14 @@ export default function ResisterForm() {
         </CardHeader>
         <CardContent>
           <div className="grid gap-4">
-            <div className="grid grid-cols-2 gap-4">
-              <div className="grid gap-2">
-                <Label htmlFor="first-name">First name</Label>
-                <Input id="first-name" placeholder="Max" required />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="last-name">Last name</Label>
-                <Input id="last-name" placeholder="Robinson" required />
-              </div>
+            <div className="grid gap-2">
+              <Label htmlFor="name">Name</Label>
+              <Input
+                id="name"
+                placeholder="Max Robinson"
+                {...formik.getFieldProps("name")}
+              />
+              <FormikError formik={formik} name="name" />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
@@ -39,14 +97,20 @@ export default function ResisterForm() {
                 id="email"
                 type="email"
                 placeholder="m@example.com"
-                required
+                {...formik.getFieldProps("email")}
               />
+              <FormikError formik={formik} name="email" />
             </div>
             <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" />
+              <Input
+                id="password"
+                type="password"
+                {...formik.getFieldProps("password")}
+              />
+              <FormikError formik={formik} name="password" />
             </div>
-            <Button type="submit" className="w-full">
+            <Button loading={loading} type="submit" className="w-full">
               Create an account
             </Button>
           </div>
@@ -58,6 +122,6 @@ export default function ResisterForm() {
           </div>
         </CardContent>
       </Card>
-    </div>
+    </form>
   );
 }
