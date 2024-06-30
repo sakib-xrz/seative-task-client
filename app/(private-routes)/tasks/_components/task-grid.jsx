ba871f80c7dd4f0d7c56995dce8704edcb828d3d
sakib-xrz <sakibxrz21@gmail.com>
2024-06-30 12:49:30 +0600

    Refactor TaskGrid component to improve layout and task card rendering

--- app/(private-routes)/tasks/_components/task-grid.jsx
+++ app/(private-routes)/tasks/_components/task-grid.jsx
@@ -39,36 +39,51 @@ export default function TaskGrid({ tasks, refetch }) {
   };
 
   return (
-    <div className="grid gap-5 md:grid-cols-3">
-      {TASK_STATUSES.map((status) => (
-        <div
-          key={status}
-          className="rounded-md bg-accent"
-          onDrop={(e) => onDrop(e, status)}
-          onDragOver={allowDrop}
-        >
-          <div className="border-b-2 py-3 text-center font-semibold">
-            {status.replace("-", " ")}{" "}
-            <span className="text-xs text-muted-foreground">
-              ({tasks?.data?.filter((task) => task?.status === status)?.length})
-            </span>
-          </div>
-          <ScrollArea className="h-[calc(100vh-400px)]">
-            <div className="space-y-2 p-2">
-              {tasks?.data
-                ?.filter((task) => task?.status === status)
-                .map((task, index) => (
-                  <TaskCard
-                    key={task._id}
-                    task={task}
-                    refetch={refetch}
-                    index={index}
-                  />
-                ))}
+    <div>
+      <div className="hidden gap-5 md:grid md:grid-cols-3">
+        {TASK_STATUSES.map((status) => (
+          <div
+            key={status}
+            className="rounded-md bg-accent"
+            onDrop={(e) => onDrop(e, status)}
+            onDragOver={allowDrop}
+          >
+            <div className="border-b-2 py-3 text-center font-semibold">
+              {status.replace("-", " ")}{" "}
+              <span className="text-xs text-muted-foreground">
+                (
+                {tasks?.data?.filter((task) => task?.status === status)?.length}
+                )
+              </span>
             </div>
-          </ScrollArea>
-        </div>
-      ))}
+            <ScrollArea className="h-[calc(100vh-400px)]">
+              <div className="space-y-2 p-2">
+                {tasks?.data
+                  ?.filter((task) => task?.status === status)
+                  .map((task, index) => (
+                    <TaskCard
+                      key={task._id}
+                      task={task}
+                      refetch={refetch}
+                      index={index}
+                    />
+                  ))}
+              </div>
+            </ScrollArea>
+          </div>
+        ))}
+      </div>
+
+      <div className="space-y-5 md:hidden">
+        {tasks?.data?.map((task, index) => (
+          <TaskCard
+            key={task._id}
+            task={task}
+            refetch={refetch}
+            index={index}
+          />
+        ))}
+      </div>
     </div>
   );
 }
